4492030979faf6a343b832f6076279fe99646e31
nekobato <nekobato@gmail.com>
2019-11-06 15:31:41 +0900

    update: move to puppeteer

--- scripts/BunkadoLeaflet.js
+++ scripts/BunkadoLeaflet.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: false });
+const puppeteer = require('puppeteer');
 const fs = require('fs-extra');
 const { IncomingWebhook } = require('@slack/webhook');
 
@@ -15,47 +14,38 @@ async function notify(src) {
   });
 }
 
-async function scrape() {
+(async () => {
   console.log('2');
   if (!fs.existsSync(cacheFile)) await fs.writeFile(cacheFile, JSON.stringify([]));
   const histories = require(cacheFile);
   console.log('3');
 
-  try {
-    const srcs = await nightmare
-      .goto('https://tokubai.co.jp/%E6%96%87%E5%8C%96%E5%A0%82/4114')
-      .wait('a.leaflet_card')
-      .evaluate(() => {
-        const anchors = document.querySelectorAll('a.leaflet_card img');
-        let srcs = [];
-        anchors.forEach((element) => {
-          const imageSrc = element.src.replace('w=600,h=300,mc=true,wo=0,ho=0,cw=600,ch=300,aw=600', 'o=true');
-          srcs.push(imageSrc);
-        });
-        return srcs;
-      })
-      .end()
-      .then((srcs) => {
-        return srcs;
-      });
-    console.log('4');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://tokubai.co.jp/%E6%96%87%E5%8C%96%E5%A0%82/4114');
 
-    await Promise.all(srcs.map((src) => {
-      console.log('5');
-      if (histories.indexOf(src) == -1) {
-        histories.push(src);
-        console.log(`New leaflet: ${src}`);
-        return notify(src);
-      }
-    }));
-  } catch (error) {
-    throw(error);
-  }
+  const srcs = await page.evaluate(() => {
+    const anchors = document.querySelectorAll('a.leaflet_card img');
+    let srcs = [];
+    anchors.forEach((element) => {
+      const imageSrc = element.src.replace('w=600,h=300,mc=true,wo=0,ho=0,cw=600,ch=300,aw=600', 'o=true');
+      srcs.push(imageSrc);
+    });
+    return srcs;
+  });
+
+  await browser.close();
+
+  await Promise.all(srcs.map((src) => {
+    console.log('5');
+    if (histories.indexOf(src) == -1) {
+      histories.push(src);
+      console.log(`New leaflet: ${src}`);
+      return notify(src);
+    }
+  }));
 
   console.log('6');
   fs.writeFileSync(cacheFile, JSON.stringify(histories));
   console.log('Done.');
-}
-
-console.log('1');
-scrape();
+})();
